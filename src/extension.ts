e00c63c33bdd21ac2b0b538c967d9ee40e39b989
Alan Rynne <alan@rynne.es>
2019-08-18 00:50:21 +0200

    Changed helloworld command name to getFileInfo

--- src/extension.ts
+++ src/extension.ts
@@ -13,11 +13,11 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('extension.GetFileInfo', () => {
 		// The code you place here will be executed every time your command is executed
-
+		
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		vscode.window.showInformationMessage('Get file info has been called!');
 	});
 
 	context.subscriptions.push(disposable);
